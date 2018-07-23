12e6738133de20a49f15a195d5fe590c5a525ee5
Paul Lee <ctwdtw@gmail.com>
2018-07-23 12:38:58 +0800

    fix(player): fix crash when load loadPlayer
    Closes #23

--- YoutubeKit/Common/Result.swift
+++ YoutubeKit/Common/Result.swift
@@ -10,4 +10,21 @@ import Foundation
 public enum Result<T, E> {
     case success(T)
     case failed(E)
+    public var value: T? {
+        switch self {
+        case .success(let value):
+            return value
+        default:
+            return nil
+        }
+    }
+    
+    public var error: E? {
+        switch self {
+        case .failed(let error):
+            return error
+        default:
+            return nil
+        }
+    }
 }
