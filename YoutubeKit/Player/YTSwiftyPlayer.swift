76541e95f7ec27c9dcd607052adbd9b9c84cd44f
Fabien Mirault <fabien.mirault@gmail.com>
2018-08-16 11:50:58 +0200

    #Load html with correct baseUrl to be able to play every youtube videos

--- YoutubeKit/Player/YTSwiftyPlayer.swift
+++ YoutubeKit/Player/YTSwiftyPlayer.swift
@@ -243,9 +243,10 @@ open class YTSwiftyPlayer: WKWebView {
         
         guard let json = try? JSONSerialization.data(withJSONObject: parameters, options: []),
             let jsonString = String(data: json, encoding: String.Encoding.utf8),
-            let html = htmlString?.replacingOccurrences(of: "%@", with: jsonString) else { return }
+            let html = htmlString?.replacingOccurrences(of: "%@", with: jsonString),
+            let baseUrl = URL(string: "https://www.youtube.com") else { return }
         
-        loadHTMLString(html, baseURL: nil)
+        loadHTMLString(html, baseURL: baseUrl)
     }
     
     // MARK: - Private Methods
