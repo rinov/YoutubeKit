77cd1c18a0c85ebff66231660a96c4cc1c5e9edf
Seth Folley <sethfolley@gmail.com>
2019-06-23 14:34:43 -0500

    Added additional init for subscription list

--- YoutubeKit/API/Models/SubscriptionsList.swift
+++ YoutubeKit/API/Models/SubscriptionsList.swift
@@ -14,6 +14,15 @@ public struct SubscriptionsList: Codable {
     public let pageInfo: PageInfo
     public let nextPageToken: String?
     public let prevPageToken: String?
+    
+    public init(etag: String, items: [Subscription], kind: String, pageInfo: PageInfo, nextPageToken: String? = nil, prevPageToken: String? = nil) {
+        self.etag = etag
+        self.items = items
+        self.kind = kind
+        self.pageInfo = pageInfo
+        self.nextPageToken = nextPageToken
+        self.prevPageToken = prevPageToken
+    }
 }
 
 public struct Subscription: Codable {
